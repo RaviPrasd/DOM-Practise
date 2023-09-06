d1d03b3f10609a2f7fb2298eff17ca20c1febe92
Ravi Prasad <raviprasad@Ravis-MacBook-Air.local>
2023-09-06 18:55:39 +0530

    Practise of DOM

---
+++ app.js
@@ -0,0 +1,50 @@
+// DOM Manipulation
+
+// getElementById()
+
+// const title = document.getElementById("main-heading");
+// console.log(title);
+
+// getElementByClassName()
+
+// const listItem = document.getElementsByClassName("list-items");
+// console.log(listItem);
+
+// getElementByTagName
+
+// const li = document.getElementsByTagName("li");
+// console.log(li);
+
+// querySelector
+
+// const query = document.querySelector("div");
+// console.log(query);
+
+// querySelectorAll()
+
+// const newQuery = document.querySelectorAll("#main-heading");
+// console.log(newQuery);
+
+// const listItems = document.querySelectorAll(".list-items");
+
+// for (i = 0; i < listItems.length; i++) {
+//   listItems[i].style.fontSize = "2rem";
+// }
+
+// console.log(listItems);
+
+// Creating Element
+
+// const ul = document.querySelector("ul");
+// const li = document.createElement("li");
+
+// Adding Element
+// ul.append(li);
+
+// Modifying Element
+
+// li.innerText = "X-Men";
+
+// Modifying Attribute
+
+// li.setAttribute(`class`, `list-items`);
